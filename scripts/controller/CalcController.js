e8a3ae2397937d51f90f674a1f819ed2269eab4a
Alline Vieira <alline.gio@hotmail.com>
2022-11-22 19:00:35 -0300

    objetos

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -3,8 +3,41 @@ class CalcController {
     
     constructor(){
 
-        this.displayCalc = "0";
-        this.dataAtual;
+        this._displayCalc = "0";
+        this._currentDate;
+        this.initialize()
 
     }
+
+    initialize(){
+//El representa elemento
+        let displayCalcEl = document.querySelector('#display');
+        let dateEl = document.querySelector('#data');
+        let timeEl = document.querySelector('#hora');
+
+        displayCalcEl.innerHTML = "4567";
+        dateEl.innerHTML = "12/01/2140";
+        timeEl.innerHTML = "00h00";
+
+    }
+   
+    get displayCalc(){
+
+        return this._displayCalc;
+    }
+
+    set displayCalc(value){
+        this._displayCalc = value;
+    }
+
+    get currentDate(){
+
+        return this._currentDate;
+    }
+
+    set currentDate(value){
+        this._currentDate = value;
+    }
+
 }
+
